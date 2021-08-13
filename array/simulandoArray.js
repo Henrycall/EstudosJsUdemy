3b9fc3029f28c8f97e1514641c2caa691660637a
Henrycall <henrycalleraa@gmail.com>
2021-08-13 15:09:50 -0300

    simulando array com objeto

---
+++ array/simulandoArray.js
@@ -0,0 +1,11 @@
+const quaseArray = {0:'Rafael',1:'Ana',2:'Beatriz'}
+console.log(quaseArray)
+
+Object.defineProperty(quaseArray,'toString',{
+    value:function(){return Object.values(this) },
+    enumerable:false
+})
+console.log(quaseArray[0]) // SIMULANDO UM ARRAY, COM OBJETO !!!!
+
+const meuArray = ['Rafael','Vivi','Pedro']
+console.log(quaseArray.toString(),meuArray)
