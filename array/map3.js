53ec4c79da88430fe49bdd466474f5bbc5467dc3
Henrycall <henrycalleraa@gmail.com>
2021-08-13 18:56:44 -0300

    ultilizando map

--- array/map3.js
+++ array/map3.js
@@ -1,3 +1,13 @@
+Array.prototype.map2 = function(callback) {
+        const newArray = []
+        for(let i = 0 ; i< this.length ; i++){
+            newArray.push(callback(this[i],i,this))
+        }
+        return newArray
+}       
+
+
+
 const carrinho = [
     '{"nome":"Borracha","preco": 3.45}',
     '{"nome":"cardeno","preco": 13.95}',
@@ -8,5 +18,5 @@ const carrinho = [
 const converteObjeto = json => JSON.parse(json)
 const apenasPreco  = produto => produto.preco 
 
-const resultadoPreco = carrinho.map(converteObjeto).map(apenasPreco)
+const resultadoPreco = carrinho.map2(converteObjeto).map2(apenasPreco)
 console.log(resultadoPreco)
